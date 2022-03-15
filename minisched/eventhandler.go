8079967be93756ec5c859cd69f50dc3bb6d7df18
Masato Naka <masatonaka1989@gmail.com>
2022-03-15 22:03:58 +0900

    1. Find unscheduled Pods

---
+++ minisched/eventhandler.go
@@ -0,0 +1,43 @@
+package minisched
+
+import (
+    v1 "k8s.io/api/core/v1"
+    "k8s.io/client-go/informers"
+    "k8s.io/client-go/tools/cache"
+)
+
+func addAllEventHandlers(
+    sched *Scheduler,
+    informerFactory informers.SharedInformerFactory,
+) {
+    // unscheduled pod
+    informerFactory.Core().V1().Pods().Informer().AddEventHandler(
+        cache.FilteringResourceEventHandler{
+            FilterFunc: func(obj interface{}) bool {
+                switch t := obj.(type) {
+                case *v1.Pod:
+                    return !assignedPod(t) // まだどこにもアサインされていないPodにtrueを返す
+                default:
+                    return false
+                }
+            },
+            Handler: cache.ResourceEventHandlerFuncs{
+                // FilterFuncでtrueが返ってきた新しいPodにこれを実行する
+                AddFunc: sched.addPodToSchedulingQueue,
+            },
+        },
+    )
+}
+
+// assignedPod selects pods that are assigned (scheduled and running).
+func assignedPod(pod *v1.Pod) bool {
+    return len(pod.Spec.NodeName) != 0
+}
+
+func (sched *Scheduler) addPodToSchedulingQueue(obj interface{}) {
+    pod, ok := obj.(*v1.Pod)
+    if !ok {
+        return
+    }
+    sched.SchedulingQueue.Add(pod)
+}
